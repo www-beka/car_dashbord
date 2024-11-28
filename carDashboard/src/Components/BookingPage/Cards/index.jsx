5434f87b88610528ef8484084fef09df8a3cd0e1
Bekzod <bekmuradovbekzod955@gmail.com>
2024-11-28 16:36:00 +0500

    changes

--- carDashboard/src/Components/BookingPage/Cards/index.jsx
+++ carDashboard/src/Components/BookingPage/Cards/index.jsx
@@ -1,3 +1,31 @@
-import React from 'react'
+import React from "react";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+const Cards = ({ title, type, users, manual, price }) => {
+  return (
+    <>
+      <div className="cards">
+        <div className="card-top">
+          <span>
+            <h4>{title}</h4>
+            {type}
+          </span>
+          <FavoriteBorderOutlinedIcon />
+        </div>
+        <img src="" alt="" />
+        <div className="card-bottom">
+          <div className="bottom-left">
+            <img src="" alt="" />
+            <span>{users}</span>
+            <img src="" alt="" />
+            <span>{manual}</span>
+          </div>
+          <div className="bottom-right">
+            <span>{price}</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-
+export default Cards;
